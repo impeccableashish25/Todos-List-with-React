054c1d13a58c7ee4c270d3c01d94257e0a38f7be
impeccableashish25 <dubeyashish117@gmail.com>
2021-08-20 12:54:47 +0530

    Intial Commit

---
+++ src/MyComponents/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const About = () => {
+    return (
+        <div>
+            This is an about component
+            <p> This Todo list contains essential list of tasks that we need to make note of , in order to do it at the end of the day. 
+
+            </p>
+        </div>
+    )
+}
---
+++ src/MyComponents/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export const Footer = () => { 
+    return (
+        <footer className="bg-primary  py-3">
+            <p className="text-center">
+            Copyright &copy; TodosListwithReact.com
+            </p>
+        </footer>
+    )
+}
